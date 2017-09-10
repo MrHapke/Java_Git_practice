d560cba9b00044db5eff7ab76772aa6f6e217c64
PeterdHapke <phapke@marshalltown.k12.ia.us>
2017-09-10 16:09:11 -0500

    Practice Changes, Modified GUI
    GUI now hass alrger fonts, centered and stacked in the window

--- MakePhrase/src/phraseOMatic/driverGUI.java
+++ MakePhrase/src/phraseOMatic/driverGUI.java
@@ -1,14 +1,17 @@
 package phraseOMatic;
 
+import java.awt.Font;
+import java.awt.LayoutManager;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+import javax.swing.BoxLayout;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 
-
+//this is a comment to practice with git
 
 public class driverGUI extends JFrame {
 		/**
@@ -17,8 +20,8 @@ public class driverGUI extends JFrame {
 			//it wanted this
 			private static final long serialVersionUID = 1L;
 			
-			private int Height = 150; 
-			private int Width = 400; 
+			private int Height = 200; 
+			private int Width = 350; 
 			private JLabel outputField;
 		
 			
@@ -39,18 +42,30 @@ public class driverGUI extends JFrame {
 				//create the panel
 				JPanel panel = new JPanel();
 				
+				//add a layout to make it look nicer
+				LayoutManager myLayout = new BoxLayout(panel, BoxLayout.Y_AXIS);
+				panel.setLayout(myLayout);
+				
+				
+				
 				//create and attach the text field and label
 				JLabel directions = new JLabel("Press the button to generate a phrase");
+				directions.setFont(new Font("Serif", Font.BOLD, 20));
+				directions.setAlignmentX(CENTER_ALIGNMENT);
 				panel.add(directions);
 				
 				//create and attached button
 				JButton makePhraseButton = new JButton("Push Me!");
 				ActionListener newPhrase = new generateNewPhrase();
 				makePhraseButton.addActionListener(newPhrase);
+				makePhraseButton.setFont(new Font("Serif", Font.BOLD, 25));
+				makePhraseButton.setAlignmentX(CENTER_ALIGNMENT);
 				panel.add(makePhraseButton);
 				
 				//add the output label
 				outputField = new JLabel(); 
+				outputField.setFont(new Font("Serif", Font.ITALIC, 20));
+				outputField.setAlignmentX(CENTER_ALIGNMENT);
 				panel.add(outputField);
 				
 				
